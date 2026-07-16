fc0cca93f58983335e696e9a7701b4a79b2c4c3f
369rahasya-a11y <369rahasya@gmail.com>
2026-07-16 14:42:07 +0530

    Update platforms to exclude Instagram
    Removed 'instagram' from the list of platforms.

--- src/commands/publishAsset.ts
+++ src/commands/publishAsset.ts
@@ -18,7 +18,7 @@ const publishers = {
   threads: new ThreadsPublisher(),
 };
 
-const PLATFORMS: Platform[] = ["facebook", "instagram", "threads"];
+const PLATFORMS: Platform[] = ["facebook", "threads"];
 
 /**
  * Parse --id=123 from command line args.
